ec7e5b7085a9d9153328a0b871fbd4d4490f4c14
kstanioDev <juliengeorget.jg@gmail.com>
2015-08-03 01:41:36 +0200

    modification dash

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,8 +2,9 @@
 @section('content')
     <section class="dash-page">
         <h1>Dashboard</h1>
+
         <div class="table-responsive">
-            <table class="table table-hover">
+            <table class="table table-hover table-bordered">
                 <thead>
                 <tr>
                     <th>Statut</th>
@@ -14,45 +15,39 @@
                     <th>Supprimer</th>
                 </tr>
                 </thead>
-                <tfoot>
-                <tr>
-                    <th>Statut</th>
-                    <th>Titre</th>
-                    <th>Date de début et fin</th>
-                    <th>Mots clefs</th>
-                    <th>Changer le statut</th>
-                    <th>Supprimer</th>
-                </tr>
-                </tfoot>
                 <tbody>
                 @foreach($posts as $post)
-                    <tr>
-                        <td>{{$post->status}}</td>
-                        <td><a href="{{url('post/'.$post->id.'/edit')}}">{{$post->title}}</a></td>
-                        <td>{{$post->date_start}} et {{$post->date_end}}</td>
-                        <td>
-                            @foreach($post->tags as $tag)
-                                <a href="{{url('tag/'.$tag->id)}}">{{ucfirst($tag->name)}}</a>
-                            @endforeach
-                        </td>
-                        <td>
-                            {!! Form::open(['url'=>'status/'.$post->id]) !!}
-                            {!! Form::hidden('_method','put')!!}
-                            @if($post->status =='publish')
-                                {!! Form::submit('unpublish', ['class'=>'btn btn-warning', 'name'=>'status']) !!}
-                            @else
-                                {!! Form::submit('publish', ['class'=>'btn btn-warning', 'name'=>'status']) !!}
+                    @if($post->status == 'publish')
+                        <tr class="success">
+                    @else
+                        <tr class="info">
                             @endif
-                            {!! Form::close()!!}
-                        </td>
-                        <td>
-                            {!! Form::open(['url'=>'post/'.$post->id]) !!}
-                            {!! Form::hidden('_method','DELETE')!!}
-                            {!! Form::submit('Supprimer', ['class'=>'btn btn-danger']) !!}
-                            {!! Form::close()!!}
-                        </td>
-                    </tr>
-                @endforeach
+                            <td>{{$post->status}}</td>
+                            <td><a href="{{url('post/'.$post->id.'/edit')}}">{{$post->title}}</a></td>
+                            <td>{{$post->date_start}} et {{$post->date_end}}</td>
+                            <td>
+                                @foreach($post->tags as $tag)
+                                    <a href="{{url('tag/'.$tag->id)}}">{{ucfirst($tag->name)}}</a>
+                                @endforeach
+                            </td>
+                            <td>
+                                {!! Form::open(['url'=>'status/'.$post->id]) !!}
+                                {!! Form::hidden('_method','put')!!}
+                                @if($post->status =='publish')
+                                    {!! Form::submit('unpublish', ['class'=>'btn btn-warning', 'name'=>'status']) !!}
+                                @else
+                                    {!! Form::submit('publish', ['class'=>'btn btn-warning', 'name'=>'status']) !!}
+                                @endif
+                                {!! Form::close()!!}
+                            </td>
+                            <td>
+                                {!! Form::open(['url'=>'post/'.$post->id]) !!}
+                                {!! Form::hidden('_method','DELETE')!!}
+                                {!! Form::submit('Supprimer', ['class'=>'btn btn-danger']) !!}
+                                {!! Form::close()!!}
+                            </td>
+                        </tr>
+                        @endforeach
                 </tbody>
             </table>
             <nav class="navigation">
--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -25,34 +25,7 @@
 <![endif]-->
 
 <div id="wrapper">
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <a class="navbar-brand" href="{{url('admin/')}}">Admin ConfPHP</a>
-        </div>
-        <!-- Top Menu Items -->
-        <ul class="nav navbar-right top-nav">
-            <li class="dropdown">
-                <a href="{{url('/admin')}}"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-            </li>
-            <li class="dropdown"><a href="/"><i class="fa fa-fw fa-chevron-circle-left"></i> Retour au site</a></li>
-            <li class="dropdown"><a href="{{url('auth/logout')}}">Deconnexion</a></li>
-            </li>
-        </ul>
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul class="nav navbar-nav side-nav">
-                <li>
-                    <a href="{{url('/post/create')}}"><i class="fa fa-fw fa-plus"></i> Ajouter une conférence</a>
-                </li>
-                <li>
-                    <a href="{{url('admin/')}}"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-                </li>
-                <li>
-                    <a href="{{url('comment/')}}"><i class="fa fa-fw fa-comment-o"></i> Commentaires</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+@include('dashboard.partials.menu')
     <div id="page-wrapper">
 
         <div class="container-fluid">
